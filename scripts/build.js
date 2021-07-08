b98c0485856fc472765eb55ef7738bfbfa3cb4d8
Snowflake107 <46562212+Snowflake107@users.noreply.github.com>
2021-07-08 15:47:35 +0545

    scripts: update platform check

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,17 @@
 
 const { exec } = require("pkg");
 
-const OUTPUT_NAME = `nexus-${process.platform === "win32" ? "windows" : process.platform}-${process.arch}${process.platform === "win32" ? ".exe" : ""}`;
+const getPlatform = () => {
+    switch(process.platform) {
+        case "win32":
+            return "windows";
+        case "darwin":
+            return "mac";
+        default:
+            return process.platform;
+    }
+};
+const OUTPUT_NAME = `nexus-${getPlatform()}-${process.arch}${process.platform === "win32" ? ".exe" : ""}`;
 const OUTPUT_PATH = `${__dirname}/../dist/${OUTPUT_NAME}`;
 
 // build the binary file
